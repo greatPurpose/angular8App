595a7b07e53a691eadbb51c52060e573095ef534
greatPurpose <happy.goodday@outlook.com>
2019-10-31 11:53:42 +0900

    user.ts added

---
+++ src/app/core/models/user.ts
@@ -0,0 +1,29 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    password: string;
+    degree: string;
+    employment: string;
+    income: number;
+    savings: number;
+    debt: number;
+    age: number;
+    zipcode: string;
+    haveVehicle: boolean;
+    haveHouse: boolean;
+    havePet: boolean;
+    have_health_cover: string;
+    have_vision_cover: string;
+    have_dental_cover: string;
+    have_life_cover: string;
+    have_longtermdisability_cover: string;
+    have_shorttermdisability_cover: string;
+    have_accident_cover: string;
+    have_criticalillness_cover: string;
+    have_auto_cover: string;
+    have_homeowner_cover: string;
+    have_renters_cover: string;
+    have_pet_cover: string;
+    terms: boolean;
+}
