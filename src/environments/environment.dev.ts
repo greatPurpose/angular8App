984e99a4f63af8037a5d8667563bca14828ecc37
greatPurpose <happy.goodday@outlook.com>
2019-11-16 16:33:53 +0900

    environment.dev.ts updated

--- src/environments/environment.dev.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiGetScoreUrl: 'https://api-dev.withvero.com/api/score/get_score/',
-  apiSignupUrl: 'https://api-dev.withvero.com/api/user/create/',
-  apiEnterCodeUrl: 'https://api-dev.withvero.com/api/user/enter_code/',
-};
